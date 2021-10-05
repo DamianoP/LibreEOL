7c927b0cc82d4ade6597bd431dfdfc60bcb92df4
abh0306 <33661374+abh0306@users.noreply.github.com>
2021-10-05 23:51:59 +0200

    Add files via upload

---
+++ questionTypes/AT_OC.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * File: AT_OC.php
+ * User: Anis
+ * Date: 02/03/2021
+ * Desc: Class for answer in On Click questions
+ */
+
+class AT_OC extends Answer {
+
+    public function printAnswerEditForm($action){
+        global $config;
+
+        $db = new sqlDB();
+        if(!($db->qSelect('Languages')) || !($this->allLangs = $db->getResultAssoc('idLanguage'))){
+            die($db->getError());
+        }
+
+        $this->printAnswerTabsAndTextareas();
+        ?>
+
+        <div class="clearer bSpace"></div>
+
+        <!-- Print all other answer's info -->
+        <?php $this->printAnswerInfoEditForm($action) ?>
+
+        <!-- Print buttons for answer panel -->
+        <?php $this->printAnswerEditButtons($action); ?>
+
+        <div class="clearer"></div>
+
+        <?php $this->printAnswerTypeLibrary(); ?>
+
+        <?php
+    }
+
+    public function printAnswerInfoEditForm($action){
+        global $log;
+        ?>
+
+        <dl class="dropdownInfo scoreOC tSpace right" id="answerScore">
+            <dt class="tSpace writable">
+                <?php
+                $score = $this->get('score');
+                if($score == 0 || $score == null || !isset($score)){
+                    echo "<span>Nessuna Posizione<span class='value'>0</span></span>";
+                }
+                else {
+                    $position = explode("*",$score)[0];
+                    echo"<span>".$position."<span class='value'>".$score."</span></span>";
+                }
+                ?>
+            </dt>
+            <dd>
+                <ol>
+                    <?php
+                    session_start();
+                    if(isset($_SESSION['counter'])){
+                        $numberItemOC = $_SESSION['counter'];
+                        $scoreSingle = round(1/($numberItemOC-1),2);
+                        echo "<li>".ttNoPosOC."<span class=\"value\">0</span></li>";
+                        if($numberItemOC != 0) {
+                            for ($i = 1; $i < $numberItemOC; $i++) {
+                                echo "<li>" . $i . "<span class=\"value\">" . $i . "*" . $scoreSingle . "</span></li>";
+                            }
+                        }
+                    }
+                    unset($element);
+                ?>
+                </ol>
+            </dd>
+        </dl>
+
+
+        <label class="right tSpace"><?= ttPosition ?> :</label><div class="clearer"></div>
+        <div class="clearer bSpace"></div>
+
+        <?php
+    }
+
+    public function getAnswerRowInTable(){
+        $position = explode("*", $this->get('score'))[0];
+        return array($position, $this->get('translation'), $this->get('idAnswer'));
+    }
+
+    public function getAnswerScore(){
+        return $this->get('score');
+    }
+    public function getScoreFromGivenAnswer(){
+
+    }
+}
---
+++ questionTypes/QT_OC.php
@@ -0,0 +1,364 @@
+<?php
+/**
+ * File: QT_OC.php
+ * User: Anis
+ * Date: 02/03/2021
+ * Desc: Class for OnClick questions
+ */
+
+class QT_OC extends Question {
+
+    public function createNewQuestion(){
+        $idQuestion = parent::createNewQuestion();
+        return $idQuestion;
+    }
+
+    public function printQuestionEditForm($action, $readonly){
+        global $config, $user;
+
+        $db = new sqlDB();
+        if(!($db->qSelect('Languages')) || !($this->allLangs = $db->getResultAssoc('idLanguage'))){
+            die($db->getError());
+        }
+        ?>
+        <ul xmlns="http://www.w3.org/1999/html">
+            <li><a id="questionTab" href="#question-tab"><?= ttQuestion ?></a></li>
+            <?php if($action == 'show') echo '<li><a id="answersTab" href="#answers">'.ttAnswers.'</a></li>'; ?>
+        </ul>
+
+        <div id="question-tab">
+
+            <!-- Print question language tabs and textareas user by ckeditor -->
+            <?php $this->printQuestionTabsAndTextareas($action); ?>
+
+            <!-- Print question's extras list -->
+            <?php $this->printQuestionExtraForm($action); ?>
+
+            <div class="clearer bSpace"></div>
+
+            <!-- Print hidden field for question's type (OC) -->
+            <input type="hidden" id="questionType" value="OC">
+
+            <!-- Print all other question's info -->
+            <?php $this->printQuestionInfoEditForm($action, $readonly) ?>
+
+            <div class="clearer bSpace"></div>
+
+            <!-- Print buttons for question panel -->
+            <?php $this->printQuestionEditButtons($action, $readonly); ?>
+
+            <div class="clearer"></div>
+
+        </div>
+
+        <?php if($action == 'show'){ ?>
+            <div id="answers">
+            <?php
+                $db = new sqlDB();
+                $db1 = new sqlDB();
+                if (!($db->qSelect("Answers", "fkQuestion", $this->get('idQuestion'))) || !($answersIdItem = $db->getResultAssoc())) {
+                    if ($db->getError() != null && $db->getError() != "")
+                        die($db->getError());
+                }
+                else {
+                    if (count($answersIdItem) > 0) {
+                        foreach ($answersIdItem as $item) {
+                            if (!($db1->qSelect("TranslationAnswers", "fkAnswer", $item['idAnswer'])) || !($answersTranslationItem = $db1->getResultAssoc())) {
+                                if ($db1->getError() != null && $db1->getError() != "")
+                                    die($db1->getError());
+                            }
+                            else {
+                                foreach ($answersTranslationItem as $element) {
+                                    $data = $element['translation'];
+                                }
+                                unset($element, $data);
+                            }
+                        }
+                        unset($item);
+                    }
+                }
+                if (!($db->qSelect("TranslationQuestions", "fkQuestion", $this->get('idQuestion'))) || !($translations = $db->getResultAssoc())) {
+                    die($db->getError());
+                }
+                else {
+                    $text = $this->get('translation');
+                    $pattern = "/<\s* input [^>]+ >/xi";
+                    $counter = 1;
+                    $text = preg_replace_callback($pattern, function($m) use (&$counter) {
+                        return "<span class=\"droptarget\" style=\"pointer-events: none\"></span>" . "<sub><font color='0f4c81'>" . $counter++ . "</font></sub>";
+                        }, $text);
+                    session_start();
+                    $_SESSION['counter'] = $counter;
+                    echo "<div class='questionTextOC'>
+                            <h3>".ttQuestion."</h3>
+                            <hr>
+                            <p>".$text."</p>
+                          </div>";
+                }
+                ?>
+                <div class="bSpace" id="answersTableContainer">
+                    <tr>
+                        <td><?ttQuestion?></td>
+                    </tr>
+                </div>
+
+                <div class="bSpace" id="answersTableContainer">
+                    <div class="smallButtons">
+                        <div id="newAnswer_OC">
+                            <img class="icon" src="<?= $config['themeImagesDir'].'new.png' ?>"/><br/>
+                            <?= ttNew ?>
+                        </div>
+                    </div>
+
+                    <?php $this->printAnswersTable($this->get('idQuestion'), $_SESSION['idSubject']) ?>
+
+                </div>
+
+                <div class="clearer"></div>
+                <a class="button normal left rSpace tSpace" onclick="closeQuestionInfo(true);"><?= ttExit ?></a>
+                <div class="clearer"></div>
+            </div>
+
+            <?php
+        }
+        $this->printQuestionTypeLibrary();
+        echo '<script> initialize_OC(); </script>';
+    }
+
+    public function printAnswersTable($idQuestion, $idSubject){
+        ?>
+
+        <table id="answersTable" class="stripe hover">
+            <thead>
+            <tr>
+                <th class="aScore"><?= ttPosition ?></th>
+                <th class="aText"><?= ttText ?></th>
+                <th class="aAnswerID">answerID</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $db = new sqlDB();
+            if($db->qAnswerSet($idQuestion, null, $idSubject)){
+                while($answer = $db->nextRowAssoc()){
+                    $position = explode("*", $answer['score'])[0];
+                    echo '<tr>
+                              <td>'.$position.'</td>
+                              <td>'.strip_tags($answer['translation']).'</td>
+                              <td>'.$answer['idAnswer'].'</td>
+                          </tr>';
+                }
+            }
+            ?>
+            </tbody>
+        </table>
+        <?php
+    }
+
+    public function printQuestionPreview(){
+        global $config;
+        $db = new sqlDB();
+        if(($db->qAnswerSet($this->get('idQuestion'), $this->get('fkLanguage'), $_SESSION['idSubject'])) && ($answerSet = $db->getResultAssoc())){
+
+            $questionAnswers = '';
+            shuffle($answerSet);
+            foreach($answerSet as $answer){
+                $class = '';
+                $idQ = $this->get('idQuestion');
+                if($answer['fkLanguage'] != $this->get('fkLanguage'))
+                    $class = 'mainLang';
+                $questionAnswers .= '<span idQ = "'.$idQ.'" value="'.$answer['idAnswer'].'" class = "answerOC">
+                '.$answer['translation'].'
+                </span>';
+            }
+
+            // -------  Add extra buttons  ------- //
+            $extra = '';
+            if(strpos($this->get('extra'), 'c') !== false)
+                $extra .= '<img class="extraIcon calculator" src="'.$config['themeImagesDir'].'QEc.png'.'">';
+            if(strpos($this->get('extra'), 'p') !== false)
+                $extra .= '<img class="extraIcon periodicTable" src="'.$config['themeImagesDir'].'QEp.png'.'">';
+            ?>
+
+            <div class="questionTest" value="<?= $this->get('idQuestion') ?>" type="OC">
+                <?php
+                $text = $this->get('translation');
+                $pattern = "/<\s* input [^>]+ >/xi";
+                $dropId = $this->get('idQuestion');
+                $regex = preg_replace($pattern, "<span class=\"droptarget\" value=$dropId idAns=\"\"></span>", $text);
+                ?>
+
+                <div class="questionText"><div class="questionSubTextOC"><?= $regex ?></div><div class="infoButtonOCContainer"><img class="infoButtonOC" src="themes/default/images/help.png"></div></div>
+                <div class="questionAnswers"><?= $questionAnswers ?></div>
+            </div>
+
+            <?php
+        }
+        $this->printQuestionTypeLibrary();
+        echo '<script> main_OC(); </script>';
+    }
+
+    public function printQuestionInTest($idSubject, $answered, $extras){
+        global $config;
+        $db = new sqlDB();
+        if(($db->qAnswerSet($this->get('idQuestion'), $this->get('fkLanguage'), $idSubject)) && ($answerSet = $db->getResultAssoc())){
+
+            $questionAnswers = '';
+            shuffle($answerSet);
+            $idQ = $this->get('idQuestion');
+            foreach($answerSet as $answer){
+                $questionAnswers .= '<span idQ = "'.$idQ.'" value="'.$answer['idAnswer'].'" class = "answerOC">
+                                         '.$answer['translation'].'
+                                     </span>';
+            }
+
+            // -------  Add extra buttons  ------- //
+            $extra = '';
+            if(strpos($this->get('extra'), 'c') !== false){
+                $extras['calculator'] = true;
+                $extra .= '<img class="extraIcon calculator" src="'.$config['themeImagesDir'].'QEc.png'.'">';
+            }
+            if(strpos($this->get('extra'), 'p') !== false){
+                $extras['periodicTable'] = true;
+                $extra .= '<img class="extraIcon periodicTable" src="'.$config['themeImagesDir'].'QEp.png'.'">';
+            }
+            ?>
+
+            <div class="questionTest" value="<?= $this->get('idQuestion') ?>" type="OC">
+                <?php
+                $text = $this->get('translation');
+                $pattern = "/<\s* input [^>]+ >/xi";
+                $regex = preg_replace($pattern, "<span class=\"droptarget\" value=$idQ idAns=\"\"></span>", $text)
+                ?>
+                <div class="questionText"><div class="questionSubTextOC"><?= $regex ?></div><div class="infoButtonOCContainer"><img class="infoButtonOC" src="themes/default/images/help.png"></div></div>
+                <div class="questionAnswers"><?= $questionAnswers ?></div>
+            </div>
+
+            <?php
+            $this->printQuestionTypeLibrary();
+            echo '<script> main_OC(); </script>';
+            return $extras;
+        }else{
+            die($db->getError());
+        }
+
+    }
+
+    public function printQuestionInCorrection($idSubject, $answered, $scale, $lastQuestion){
+        global $config;
+
+        $questionAnswers = '';
+        $questionScore = 0;
+        $correctCounter = 0;
+        $questionClass = 'emptyQuestion';
+        $correctAnswer = array();
+        $answeredText = array();
+
+        $db = new sqlDB();
+        if(($db->qAnswerSet($this->get('idQuestion'), null, $idSubject)) && ($answerSet = $db->getResultAssoc('idAnswer'))){
+            foreach($answerSet as $idAnswer => $answer){
+                $answerdClass = "";
+                $scoreNoPosition = explode("*", $answer['score'])[1];
+                $position = explode("*", $answer['score'])[0];
+                $right_wrongClass = ($scoreNoPosition > 0) ? 'rightAnswer' : 'wrongAnswer';
+
+                if(in_array($idAnswer, $answered))
+                {
+                    $answeredPos = array_search($idAnswer, $answered);
+                    $answeredText[$answeredPos]=$answer['translation'];
+                    $answerdClass = 'answered';
+                }
+                if($position>0) {
+                    $correctAnswer[$position - 1] = $idAnswer;
+                    if ($correctAnswer[$position - 1] == $answered[$position - 1]) {
+                        $questionScore += round(($scoreNoPosition * $scale), 2);
+                        $correctCounter++;
+                    }
+                }
+                $questionAnswers .= '<div class="'.$answerdClass.'">
+                                         <span value="'.$idAnswer.'" class="responseOC '.$right_wrongClass.'"></span>
+                                         <label>'.$answer['translation'].'</label>
+                                         <label class="score">'.number_format(round($scoreNoPosition * $scale, 2), 2).'</label>
+                                     </div>';
+            }
+            if(count($answered)==$correctCounter){
+                $questionScore = 1*$scale;
+            }
+            $questionAnswers .= '<label class="questionScore">'.number_format($questionScore, 2).'</label>
+                                 <div class="clearer"></div>';
+
+            $questionClass = ($questionScore > 0) ? 'rightQuestion' : 'wrongQuestion';
+            $counter = 1;
+            $pattern = "/<\s* input [^>]+ >/xi";
+            $regex = preg_replace_callback($pattern, function($m) use (&$counter, $answeredText, $correctAnswer, $answered) {
+                $pos = $counter-1;
+                $counter++;
+                if($correctAnswer[$pos] == $answered[$pos]) {
+                    return "<span class=\"droptarget\" style=\"pointer-events: none\">$answeredText[$pos]</span>";
+                }
+                else{
+                    if($answeredText[$pos]==""){
+                        return "<span class=\"droptarget\" style=\"pointer-events: none; background-color: #ffffd1\">$answeredText[$pos]</span>";
+                    }
+                    else {
+                        return "<span class=\"droptarget\" style=\"pointer-events: none; background-color: #ff5047\">$answeredText[$pos]</span>";
+                    }
+                }
+            }, $this->get('translation'));
+            $responseScore = ($questionScore > 0) ? $questionScore : 0;
+
+            ?>
+
+            <div class="questionTest <?= $questionClass.' '.$lastQuestion ?>" value="<?= $this->get('idQuestion') ?>" type="OC">
+                <div class="questionText" onclick="showHide(this);">
+                    <span class="responseQuestion"></span>
+                    <?=
+                        $regex
+                    ?>
+                    <span class="responseScore"><?= number_format($responseScore, 2); ?></span>
+                </div>
+                <div class="questionAnswers hidden"><?= $questionAnswers ?></div>
+            </div>
+
+            <?php
+        }else{
+            die(ttEAnswers);
+        }
+    }
+
+    public function printQuestionInView($idSubject, $answered, $scale, $lastQuestion){
+        $this->printQuestionInCorrection($idSubject, $answered, $scale, $lastQuestion);
+    }
+
+    public function getScoreFromGivenAnswer(){
+        $score = 0;
+
+        $answer = json_decode(stripslashes($this->get('answer')), true);
+        $idQuestion = $this->get('idQuestion');
+
+        $db = new sqlDB();
+
+        $totalCorrectCheck = 0;
+        if(($db->qSelect("Answers", "fkQuestion", $idQuestion)) && ($answerSet = $db->getResultAssoc())){
+            foreach ($answerSet as $item) {
+                if ($item['score'] != 0) {
+                    $ansScore = explode("*", $item[score])[1];
+                    $position = explode("*", $item[score])[0];
+                    $answerChecker = $answer[($position - 1)];
+                    if ($answerChecker == $item['idAnswer']) {
+                        $score += $ansScore;
+                        $totalCorrectCheck+=1;
+                    }
+                }
+            }
+            unset($item, $idItemArray, $element);
+        } else {
+            $error = $db->getError();
+            die($error);
+        }
+        if($totalCorrectCheck == count($answerSet)) {
+            $score = 1;
+        }
+        return $score;
+    }
+}
